9d8291338bbc1302770e3d9067458fd3c0ee2cd5
Artak1999 <dashyan.1999@mail.ru>
2022-07-11 22:36:04 +0500

    Interesting.

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -2,20 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class BasicSwitch {
     public static void main(String[] args) {
-        int a = 20;
+        int a = 21;
         switch (a) {
-            case 7:
-                System.out.println("seven");
-                break;
-            case 10:
-                System.out.println("ten");
-                break;
-            case 20:
-                System.out.println("twenty");
-                break;
-            default:
-                System.out.println("default");
-                break;
+            case 7: System.out.println("seven");break;
+            case 10: System.out.println("ten");break;
+            case 20: System.out.println("twenty");break;
+            default: System.out.println("default");break;
         }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -4,7 +4,7 @@ public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
         switch (month){
             case 1:System.out.println(31);break;
-            case 2:System.out.println(28 + "or" + 29);break;
+            case 2:System.out.println(28);break;
             case 3:System.out.println(31);break;
             case 4:System.out.println(30);break;
             case 5:System.out.println(31);break;
@@ -15,7 +15,7 @@ public class SwitchDaysInMonthPrinter {
             case 10:System.out.println(31);break;
             case 11:System.out.println(30);break;
             case 12:System.out.println(31);break;
-            default:System.out.println("wrong number!");break;
+            default:System.out.println("invalid date!");
         }
     }
 }
